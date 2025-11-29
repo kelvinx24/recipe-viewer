39ea539f938c628b3c4a1c341f9699bbec80b6e4
Kelvin <kelvinjust4school@gmail.com>
2025-11-28 19:59:43 -0500

    Base positive and error cases, base fetcher implementation, type definition for responses

--- src/interfaces/Types.interface.ts
+++ src/interfaces/Types.interface.ts
@@ -16,3 +16,68 @@ export interface SimplifiedSearchFood {
   fdcId: number;
   description: string;
 }
+
+export interface SpoonacularIngredient {
+  id: number;
+  aisle: string;
+  name: string;
+  nameClean: string;
+  original: string;
+  originalName: string;
+  amount: number;
+  unit: string;
+  measures: {
+    us: {
+      amount: number;
+      unitShort: string;
+      unitLong: string;
+    };
+    metric: {
+      amount: number;
+      unitShort: string;
+      unitLong: string;
+    };
+  }
+}
+
+export interface SpoonacularNutrient {
+  name: string;
+  amount: number;
+  unit: string;
+  percentOfDailyNeeds: number;
+}
+
+export interface SpoonacularNutritionInfo {
+  nutrients: SpoonacularNutrient[];
+  properties: {
+    nutritionScore: number;
+  };
+
+  ingredients: {
+    id: number;
+    name: string;
+    amount: number;
+    unit: string;
+    nutrients: SpoonacularNutrient[];
+  }[];
+
+  caloricBreakdown: {
+    percentProtein: number;
+    percentFat: number;
+    percentCarbs: number;
+  };
+  weightPerServing: {
+    amount: number;
+    unit: string;
+  };
+}
+
+export interface SpoonacularRecipeInformation {
+  title: string;
+  healthScore: number;
+  servings: number;
+  instructions: string;
+
+  extendedIngredients: SpoonacularIngredient[];
+  nutrition: SpoonacularNutritionInfo;
+}
